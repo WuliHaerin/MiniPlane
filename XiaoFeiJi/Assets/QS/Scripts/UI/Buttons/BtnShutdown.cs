a3e437083ab1825fa166c317835c96db48be6e3c
CZY <chenzhengyoung@gmail.com>
2025-01-22 18:56:58 +0800

    1.0

---
+++ XiaoFeiJi/Assets/QS/Scripts/UI/Buttons/BtnShutdown.cs
@@ -0,0 +1,20 @@
+
+
+using UnityEngine;
+using System.Collections;
+
+public class BtnShutdown : MonoBehaviour {
+
+	public Sprite spriteClick;
+
+	void OnMouseDownMobile() {		
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#elif UNITY_WEBPLAYER
+			string webplayerQuitURL = "http://www.quadrantestudio.com";
+			Application.OpenURL(webplayerQuitURL);
+		#else
+			Application.Quit();
+		#endif
+	}
+}
---
+++ XiaoFeiJi/Assets/QS/Scripts/UI/StartGame.cs
@@ -0,0 +1,20 @@
+
+
+using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+	
+	private Manager manager;
+
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.Find ("Scripts").GetComponent<Manager> ();	
+	}
+	
+	void OnMouseDownMobile() {
+		manager.startGame = true;
+		if (Time.timeScale < 1)
+			Time.timeScale = 1;
+	}
+}
